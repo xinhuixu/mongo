b2e9a3e75f33557d03ecb4eeb5c3b24567953aa6
xinhuixu <x.xu.1102@gmail.com>
2017-02-05 12:46:46 -0500

    schema

--- db_builder.py
+++ db_builder.py
@@ -1,13 +1,32 @@
 import csv
+from pymongo import MongoClient
 
+#csv parsing
 po = open("peeps.csv")
-pd = csv.DictReader(po)
-
+peeps = csv.DictReader(po)
 co = open("courses.csv")
-cd = csv.DictReader(pd)
+courses = csv.DictReader(co)
 
-for k in pd:
-    print k
+#build peeps & courses data
+students = []
+# [ {'name': 'alison', 'id': '10', 'age': '23',
+#   'courses': [
+#               {'code': 'systems',
+#               'mark': '85'},
+#               {'code': 'softdev',
+#               'mark': '80'}
+#              ]
+#   }, {'name': 'tINI'...}, {...} ]
+for p in peeps:
+    
 
-for k in cd:
-    print k
+
+#mongoz !!LOOPBACK FOR NOW CHANGE TO HOMER LATER
+server = MongoClient('127.0.0.1')
+#'declare' single database (db name = teamname)
+db = server.XL
+#'declare' single collection
+coll = db.coll
+#create a single document for each student
+for doc in students:
+    coll.insert_one(doc)
