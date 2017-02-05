cc566ad651eb122fe46a53106b404b6c2eca36ec
xinhuixu <x.xu.1102@gmail.com>
2017-02-05 15:40:09 -0500

    enable homer

--- db_builder.py
+++ db_builder.py
@@ -1,12 +1,12 @@
 import csv
-#from pymongo import MongoClient
+from pymongo import MongoClient
 
 #mongoz !!LOOPBACK FOR NOW CHANGE TO HOMER LATER
-#server = MongoClient('127.0.0.1')
+server = MongoClient('149.89.150.100') 
 #'declare' single database (db name = teamname)
-#db = server.XL
+db = server.XL
 #'declare' single collection
-#coll = db.coll
+coll = db.coll
 
 
 #csv parsing
@@ -40,8 +40,8 @@ for c in courses:
             courses_dict['code'] = c['code']
             courses_dict['mark'] = c['mark']
             item['courses'].append(courses_dict)
-print students
+#print students
 
 #create a single document for each student
-#for doc in students:
- #   coll.insert_one(doc)
+for doc in students:
+    coll.insert_one(doc)
