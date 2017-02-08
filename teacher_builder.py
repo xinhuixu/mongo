75475ef93c77e5f990457311fbbd64cf98665f57
xinhuixu <x.xu.1102@gmail.com>
2017-02-07 21:11:35 -0500

    Update teacher_builder.py

--- teacher_builder.py
+++ teacher_builder.py
@@ -5,12 +5,28 @@ server = MongoClient('149.89.150.100')
 db = server.XL
 coll = db.coll.find()
 
+#display name, id, average                                             
 for p in coll:
-    courses =  p['courses']    
+    courses =  p['courses']
+    grade_sum = 0
+    count = 0
     for c in courses:
-        print c
+        grade_sum += int( c['mark'] )
+        count += 1
+    average = grade_sum / (1.0 * count)
+    print p['name'], p['id'], average
 
+#declare teachers collection                                           
+teachers = db.teachers
+td = csv.DictReader(open('teachers.csv'))
+for t in td:
+    print t
+    
+#Each teacher document should contain their name, class, period and st\
+#udents in that class                                                   
+#The student data should be a list of ids that match the ids of studen\
+#ts from the other collection.                                          
 
-
-#teachers = db.teachers
+#teacher = ...                                                         
+#teachers.insert_one( teacher )   
 
